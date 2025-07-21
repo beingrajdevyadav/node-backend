6373c74385606d710c875c0c1e4cfb956def7c2d
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-21 17:02:08 +0530

    api endpoints added.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from "express";
 import cors from "cors";
 import { configDotenv } from "dotenv";
+// Importing data from local files
+import posts from '../data/posts.js';
+import users from '../data/users.js';
+import products from '../data/products.js';
+import quotes from '../data/quotes.js';
+import recipes from '../data/recipes.js';
 
 configDotenv();
 
@@ -10,10 +16,51 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Jai Shree Shyam!");
 });
 
-app.listen(port, ()=>{
+// Endpoint to get all posts
+app.get('/api/v1/posts', (req, res) => {
+    res.status(200).json({
+        success: true,
+        posts
+    });
+});
+
+// Endpoint to get all users
+app.get('/api/v1/users', (req, res) => {
+    res.status(200).json({
+        success: true,
+        users
+    });
+});
+
+
+// Endpoint to get all products
+app.get('/api/v1/products', (req,res)=>{
+    res.status(200).json({
+        success: true,
+        products
+    });
+});
+
+// Endpoint to get all quotes
+app.get('/api/v1/quotes', (req,res)=>{
+    res.status(200).json({
+        success: true,
+        quotes
+    });
+});
+
+// Endpoint to get all recipes
+app.get('/api/v1/recipes', (req,res)=>{
+    res.status(200).json({
+        success: true,
+        recipes
+    });
+})
+
+app.listen(port, () => {
     console.log(`http://localhost:${port}`);
 })
