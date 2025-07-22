61cf2cd7976e930c2329df8f4989336e9101c34e
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-22 06:36:42 +0530

    indian top 10 rich data added.

---
+++ data/indian10.js
@@ -0,0 +1,86 @@
+
+ const top10Richest = [
+    {
+      "rank": 1,
+      "name": "Mukesh Ambani",
+      "age": 68,
+      "net_worth_usd_billion": 115.3,
+      "world_rank": 15,
+      "source_of_wealth": "Reliance Industries (oil, gas, retail, telecom)"
+    },
+    {
+      "rank": 2,
+      "name": "Gautam Adani",
+      "age": 63,
+      "net_worth_usd_billion": 67.0,
+      "world_rank": 24,
+      "source_of_wealth": "Adani Group (infrastructure, energy, ports)"
+    },
+    {
+      "rank": 3,
+      "name": "Shiv Nadar",
+      "age": 79,
+      "net_worth_usd_billion": 38.0,
+      "world_rank": 48,
+      "source_of_wealth": "HCL Enterprise (IT services)"
+    },
+    {
+      "rank": 4,
+      "name": "Savitri Jindal & family",
+      "age": 75,
+      "net_worth_usd_billion": 37.3,
+      "world_rank": 49,
+      "source_of_wealth": "O.P. Jindal Group (steel & power)"
+    },
+    {
+      "rank": 5,
+      "name": "Dilip Shanghvi",
+      "age": 69,
+      "net_worth_usd_billion": 26.4,
+      "world_rank": 77,
+      "source_of_wealth": "Sun Pharmaceutical Industries (pharmaceuticals)"
+    },
+    {
+      "rank": 6,
+      "name": "Cyrus Poonawalla",
+      "age": 84,
+      "net_worth_usd_billion": 25.1,
+      "world_rank": 81,
+      "source_of_wealth": "Serum Institute of India (vaccine manufacturing)"
+    },
+    {
+      "rank": 7,
+      "name": "Kumar Mangalam Birla",
+      "age": 58,
+      "net_worth_usd_billion": 22.2,
+      "world_rank": 95,
+      "source_of_wealth": "Aditya Birla Group (conglomerate: telecom, cement, finance)"
+    },
+    {
+      "rank": 8,
+      "name": "Lakshmi Mittal",
+      "age": 75,
+      "net_worth_usd_billion": 18.7,
+      "world_rank": 116,
+      "source_of_wealth": "ArcelorMittal (steel)"
+    },
+    {
+      "rank": 9,
+      "name": "Radhakishan Damani",
+      "age": 70,
+      "net_worth_usd_billion": 18.3,
+      "world_rank": 122,
+      "source_of_wealth": "Avenue Supermarts (DMart retail)"
+    },
+    {
+      "rank": 10,
+      "name": "Kushal Pal Singh",
+      "age": 93,
+      "net_worth_usd_billion": 18.1,
+      "world_rank": 124,
+      "source_of_wealth": "DLF (real estate)"
+    }
+  ];
+
+
+  export default top10Richest;
